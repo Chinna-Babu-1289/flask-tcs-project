8ab973d324e818f27c16f5a117af21acfbd7d274
chinna babu <vanarasichinnababu@gmail.com>
2022-08-06 21:28:17 +0530

    updated html css

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from enum import unique
 import re
 from flask import Flask, render_template, redirect, request, url_for, session
 from flask_sqlalchemy import SQLAlchemy
@@ -23,9 +24,10 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
+    confirmpassword = db.Column(db.String(255), nullable=False)
     date_added = db.Column(db.Date(), default=datetime.now(), nullable=False)
 
     def __repr__(self) -> str:
@@ -37,7 +39,7 @@ def formatUser(user):
         "username": user.username,
         "email": user.email,
         "password": user.password,
-        "confirmPassword": user.confirmPassword,
+        "confirmpassword": user.confirmpassword,
     }
 
 
@@ -55,11 +57,11 @@ def dashboard():
 def login():
     if request.method == 'POST':
         loginDetails = request.form
-        username = loginDetails['username']
+        # print(loginDetails)
+        email = loginDetails['email']
         password = loginDetails['password']
-        # authenticate(username, password)
         dbuser = User.query.filter_by(
-            username=username, password=password).first()
+            email=email, password=password).first()
         if dbuser:
             session['logged_in'] = True
             return redirect(url_for('dashboard'))
@@ -76,34 +78,18 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        signupDetails = request.form
+        print(signupDetails)
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
-        user = User(username=username, email=email, password=password)
+        confirmpassword = request.form['password2']
+        user = User(username=username, email=email,
+                    password=password, confirmpassword=confirmpassword)
         db.session.add(user)
         db.session.commit()
         return redirect('/login')
     return render_template('register.html')
-
-
-# @app.route('/add_user', methods=['GET', 'POST'])
-# def add_user():
-#     if request.method == 'POST':
-#         userDetails = request.json
-#         username = userDetails['username']
-#         email = userDetails['email']
-#         password = userDetails['password']
-#         confirmPassword = userDetails['confirmPassword']
-#         user = User(username=username, email=email,
-#                      password=password, confirmPassword=confirmPassword)
-#         db.session.add(user)
-#         db.session.commit()
-#         return formatUser(user)
-
-#     if request.method == 'GET':
-#         users = User.query.all()
-#         user_list = [format(user) for user in users]
-#         return {"result": user_list}
 
 
 if __name__ == '__main__':
